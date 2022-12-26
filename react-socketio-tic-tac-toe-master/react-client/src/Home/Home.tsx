7dff074a1bfc8e38658d0a0639914a5e4151fd53
lalithkumar1512 <78254801+lalithkumar1512@users.noreply.github.com>
2022-12-26 19:58:21 +0530

    Add files via upload

---
+++ react-socketio-tic-tac-toe-master/react-client/src/Home/Home.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import "./App.css";
+import { io } from "socket.io-client";
+import socketService from "../services/socketService";
+import { JoinRoom } from "../components/joinRoom";
+
+import GameContext, { IGameContextProps } from "../gameContext";
+import { Game } from "../components/game";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  redirect,
+} from "react-router-dom"; 
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1em;
+`;
+
+const WelcomeText = styled.h1`
+  margin: 0;
+  color: #8e44ad;
+`;
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default function Home() {
+  const [isInRoom, setInRoom] = useState(false);
+  const [playerSymbol, setPlayerSymbol] = useState<"x" | "o">("x");
+  const [isPlayerTurn, setPlayerTurn] = useState(false);
+  const [isGameStarted, setGameStarted] = useState(false);
+
+  const connectSocket = async () => {
+    const socket = await socketService
+      .connect("http://localhost:9000")
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+
+  useEffect(() => {
+    connectSocket();
+  }, []);
+
+  const gameContextValue: IGameContextProps = {
+    isInRoom,
+    setInRoom,
+    playerSymbol,
+    setPlayerSymbol,
+    isPlayerTurn,
+    setPlayerTurn,
+    isGameStarted,
+    setGameStarted,
+  };
+
+  return (
+    // <Router>
+    //     <Routes>
+    //       <Route path="/" element={<Login/>} />
+    //       <Route path="/Login" element={<Login />} />
+    //       <Route path="/Register" element={<Register />} />
+    //     </Routes>
+    //   </Router>
+    <GameContext.Provider value={gameContextValue}>
+      <AppContainer>
+        <WelcomeText>Welcome to Tic-Tac-Toe</WelcomeText>
+        <MainContainer>
+          {!isInRoom && <JoinRoom />}
+          {isInRoom && <Game />}
+        </MainContainer>
+      </AppContainer>
+    </GameContext.Provider>
+  );
+}
